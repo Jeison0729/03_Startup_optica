7cb300da469cb2bebc32bb0ecf5824478306305b
JeisonRuiz <farfan2@hotmail.es>
2026-06-15 21:42:26 -0500

    Update LogAuditoriaRepository.java

--- Backend_Optica/src/main/java/oft/optica/auditorias/LogAuditoriaRepository.java
+++ Backend_Optica/src/main/java/oft/optica/auditorias/LogAuditoriaRepository.java
@@ -3,6 +3,7 @@ package oft.optica.auditorias;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,8 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 
 @Repository
-public interface LogAuditoriaRepository extends JpaRepository<LogAuditoriaEntity, Integer> {
+public interface LogAuditoriaRepository extends JpaRepository<LogAuditoriaEntity, Integer>, JpaSpecificationExecutor<LogAuditoriaEntity> {
+
 
     @Query("SELECT log FROM LogAuditoriaEntity log WHERE " +
             "(:tablaAfectada IS NULL OR log.tablaAfectada = :tablaAfectada) AND " +
